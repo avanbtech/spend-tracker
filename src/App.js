5bc67fd4366486e52d3ae73513077b8006810bb1
Ava <avanbtech@gmail.com>
2021-08-24 16:38:56 -0700

    Add year filter for expenses
    - Add ExpensesFilter component and style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+//import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/newExpense/NewExpense';
 
 const App = () => {
-	const expenses = [
+    //const [enteredItem, setEnteredItem] = useState('');
+	let expenses = [
 		{ id: 'e1', title: 'Toilet paper', amount: 94.12, date: new Date(2021, 7, 14) },
 		{ id: 'e2', title: 'pasta', amount: 4.56, date: new Date(2021, 7, 14) },
 		{ id: 'e3', title: 'paper', amount: 9.2, date: new Date(2021, 8, 1) },
 		{ id: 'e4', title: 'pen', amount: 1.65, date: new Date(2021, 7, 4) }
 	];
+
+    const addExpenseHandler = (expense) => {
+        expense["id"] = 'e' + (expenses.length  + 1);
+        expenses.push(expense);
+        //setEnteredItem(...expenses);
+        console.log(expenses);
+    }
+
 	return (
 		<div>
-			<NewExpense />
+			<NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}></Expenses>
 		</div>
 	);
